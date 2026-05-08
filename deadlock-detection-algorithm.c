37ac99197d4276af1fe14d1538c397da23d5aad2
Priyanshu Sharan <priyanshu0sharan@gmail.com>
2026-05-08 18:55:09 +0530

    Implement deadlock detection algorithm in C

---
+++ deadlock-detection-algorithm.c
@@ -0,0 +1,168 @@
+#include <stdio.h>
+
+int main() {
+    int n, m, i, j, k;
+
+    printf("1WA24CS223\n");
+
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    printf("Enter the number of resources: ");
+    scanf("%d", &m);
+
+    int allocation[n][m];
+    int request[n][m];
+    int available[m];
+    int work[m];
+    int finish[n];
+    int safeSequence[n];
+
+    printf("Enter the allocation matrix:\n");
+
+    for(i = 0; i < n; i++) {
+        printf("Process %d: ", i);
+
+        for(j = 0; j < m; j++) {
+            scanf("%d", &allocation[i][j]);
+        }
+    }
+
+    printf("Enter the request matrix:\n");
+
+    for(i = 0; i < n; i++) {
+        printf("Process %d: ", i);
+
+        for(j = 0; j < m; j++) {
+            scanf("%d", &request[i][j]);
+        }
+    }
+
+    printf("Enter the available resources: ");
+
+    for(i = 0; i < m; i++) {
+        scanf("%d", &available[i]);
+        work[i] = available[i];
+    }
+
+    for(i = 0; i < n; i++) {
+
+        int allZero = 1;
+
+        for(j = 0; j < m; j++) {
+
+            if(allocation[i][j] != 0) {
+                allZero = 0;
+                break;
+            }
+        }
+
+        if(allZero)
+            finish[i] = 1;
+        else
+            finish[i] = 0;
+    }
+
+    int count = 0;
+
+    while(count < n) {
+
+        int found = 0;
+
+        for(i = 0; i < n; i++) {
+
+            if(finish[i] == 0) {
+
+                int possible = 1;
+
+                for(j = 0; j < m; j++) {
+
+                    if(request[i][j] > work[j]) {
+                        possible = 0;
+                        break;
+                    }
+                }
+
+                if(possible) {
+
+                    for(k = 0; k < m; k++) {
+                        work[k] += allocation[i][k];
+                    }
+
+                    safeSequence[count] = i;
+                    count++;
+                    finish[i] = 1;
+                    found = 1;
+                }
+            }
+        }
+
+        if(found == 0)
+            break;
+    }
+
+    int deadlock = 0;
+
+    for(i = 0; i < n; i++) {
+
+        if(finish[i] == 0) {
+            deadlock = 1;
+            break;
+        }
+    }
+
+    if(deadlock) {
+
+        printf("\nSystem is in DEADLOCK state.\n");
+
+        printf("Deadlocked processes are: ");
+
+        for(i = 0; i < n; i++) {
+
+            if(finish[i] == 0)
+                printf("P%d ", i);
+        }
+
+        printf("\n");
+    }
+    else {
+
+        printf("\nSystem is in safe state.\n");
+
+        printf("Safe Sequence is: ");
+
+        for(i = 0; i < n; i++) {
+            printf("P%d ", safeSequence[i]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+}
+/*
+OUTPUT
+1WA24CS223
+Enter the number of processes: 5
+Enter the number of resources: 3
+
+Enter the allocation matrix:
+Process 0: 0 1 0
+Process 1: 2 0 0
+Process 2: 3 0 3
+Process 3: 2 1 1
+Process 4: 0 0 2
+
+Enter the request matrix:
+Process 0: 0 0 0
+Process 1: 2 0 2
+Process 2: 0 0 0
+Process 3: 1 0 0
+Process 4: 0 0 2
+
+Enter the available resources:
+0 0 0
+
+System is in DEADLOCK state.
+Deadlocked processes are: P1 P3 P4
+*/
